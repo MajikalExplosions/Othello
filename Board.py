8ccfebcc216994cd83375fa913c1d66358c7b49a
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2020-04-21 11:39:55 -0700

    Update piece get and set methods
    I haven't updated stability and other things yet so I need to do that

--- Board.py
+++ Board.py
@@ -1,19 +1,56 @@
 class Board:
 
+    def __init__(self):
+        self.board = []
+        self.stable = []
+        self.count = [[2, 0], [2, 0]]
+        for i in range(8):
+            self.board.append([0, 0, 0, 0, 0, 0, 0, 0])
+            self.stable.append([False, False, False, False, False, False, False, False])
+        
+        self.board[3][3], self.board[4][4] = 1, 1
+        self.board[3][4], self.board[4][3] = -1, -1
+
     def gameOver(self):
+        return False
+
+    def countPieces(self, player):
+        pass
+    
+    def countStablePieces(self, player):
         pass
 
-    def countPieces(self, team):
+    def getPiece(self, location):
+        return self.board[location[0]][location[1]]
+
+    def getMoves(self, player):
         pass
 
-    def getPiece(self, x, y):
-        pass
+    def setPiece(self, location, player):
+        directions = [[0, 1], [1, 1], [1, 0], [0, -1], [-1, -1], [-1, 0], [-1, 1], [1, -1]]
+        locOpposites = []
+        for d in directions:
+            locTemp, index = [], [], 1
+            while self._inRange(location[0] + d[0] * index) and self._inRange(location[1] + d[1] * index):
+                x, y = location[0] + d[0] * index, location[1] + d[1] * index
 
-    def getMoves(self, team):
-        pass
+                if self.getPiece(x, y) == player * -1:
+                    locTemp.append((x, y))
 
-    def move(self, player, move):
-        pass
+                if self.getPiece(x, y) == player:
+                    for move in locTemp:
+                        locOpposites.append(move)
+                    break
+                if self.getPiece(x, y) == 0:
+                    break
+                
+                index += 1
+        
+        return locOpposites
 
-    def clear(self, move):
+
+    def _updateStability(self, location):
         pass
+    
+    def _inRange(self, number):
+        return number >= 0 and number <= 7
