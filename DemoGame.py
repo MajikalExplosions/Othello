e358235fba322f6819425769683c1eeafd1ef330
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2020-04-27 15:31:50 -0700

    Update DemoGame.py

--- DemoGame.py
+++ DemoGame.py
@@ -14,7 +14,7 @@ from random import random
 def main():
     b = Board()
     #Black (-1) is the good bot, white (1) is the bad bot.  Don't worry about the last three tuples because those are AI utility parameters.
-    players = [OthelloBot(-1, b, (-2, 0.75), (0.5, 0.25), (1, 0.5)), OthelloBot(1, b, (0.1, 0.1), (0.1, 0.1), (0.1, 0.1))]
+    players = [OthelloBot(-1, b, (-1, 1), (0.75, 0.5), (1, 0.25)), OthelloBot(1, b, (0.1, 0.1), (0.1, 0.1), (0.1, 0.1))]
     p = 0
     longest = 0
     while not b.gameOver():
@@ -35,5 +35,5 @@ def main():
         print("-----------------------------------------------")
     b._printBoard()
     print("White:", b.countPieces(1), " | Black:", b.countPieces(-1))
-    print("Longest turn:", longest)
+    print("Longest turn:", longest, "ms")
 main()
