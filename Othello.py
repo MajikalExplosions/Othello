e830777a02ae636b87746c1322b9f93f430c874e
nmummaneni <57919021+nmummaneni@users.noreply.github.com>
2020-04-27 23:36:43 -0700

    Update Othello.py

--- Othello.py
+++ Othello.py
@@ -16,43 +16,67 @@ def main():
     running=True
     turn = 1
     while running == True:
+        # update score board
         whiteScore = board.countPieces(1)
         blackScore = board.countPieces(-1)
         gui.updateScore(1,whiteScore)
         gui.updateScore(-1,blackScore)
-        
+
+        # flip whose turn it is
         turn*=-1
+
+        # get all the valid moves for that turn
         validMoves = board.getMoves(turn)
 
         # no valid moves, go to other player's turn
         if len(validMoves)==0:
-            gui.updateMessage(turn,True,False)
+            gui.updateMessage(turn,True,False,False)
             running = gui.clickAnywhere()
-            break
 
-        gui.updateMessage(turn,True,True)
-
-        # human player turn
-        if turn==human:
-            clickPt = gui.click()
-            if clickPt=="Quit":
-                running = False
-                break
-            else:
-                if validMoves.count(clickPt)!=0:
-                    gui.newPiece(board,turn,clickPt,True)
-                else:
-                    gui.updateMessage(turn,False,True)
-                    turn*=-1
-
-        # AI turn
+        # there are valid moves
         else:
-            sleep(1.5)
-            move = bot.getMove()
-            gui.highlightSquare(move)
-            gui.newPiece(board,turn,move,True)
-            sleep(1.5)
-            gui.unhighlightSquare()
+
+            gui.updateMessage(turn,True,True,False)
+
+            # human player turn
+            if turn==human:
+                clickPt = gui.click()
+                if clickPt=="Quit":
+                    running = False
+                    break
+                else:
+                    # selected move invalid
+                    if validMoves.count(clickPt)!=0:
+                        gui.newPiece(board,turn,clickPt,True)
+                        sleep(1.5)
+                    # selected move valid
+                    else:
+                        gui.updateMessage(turn,False,True,False)
+                        turn*=-1
+
+            # AI turn
+            else:
+                move = bot.getMove()
+                gui.highlightSquare(move)
+                gui.newPiece(board,turn,move,True)
+                sleep(1.5)
+                gui.unhighlightSquare()
+
+            # game is over
+            if board.gameOver():
+
+                # check who won
+                if board.countPieces(1) > board.countPieces(-1):
+                    color = 1
+                else:
+                    color = -1
+                gui.updateMessage(color,False,True,True)
+
+                # keep clicking until quit
+                click = gui.clickAnywhere()
+                while click:
+                    click = gui.clickAnywhere()
+                break
             
 
 main()
