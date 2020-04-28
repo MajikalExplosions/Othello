8ce456c81b4e71c204dffcd1bd504290de734291
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2020-04-28 15:14:16 -0700

    Start updating bot to use the time of the last search to decide how deep to search.

--- OthelloBot.py
+++ OthelloBot.py
@@ -1,11 +1,11 @@
 # File: OthelloBot.py
 # Written By: Joseph Liu
 # Date: 4/14/20
-# This module implements an Othello bot, using a Minimax algorithm.
+# This module implements an Othello bot, using a Minimax algorithm.  Uses time to decide depth
 
 from Minimax import Minimax
 import random
-
+from time import time
 class OthelloBot:
 
     def __init__(self, team, board, discMultiplier, mobilityMultiplier, stabilityMultiplier):
@@ -29,29 +29,15 @@ class OthelloBot:
         
         if self.d:
             return self.getDebugMove()
-        
-        #If we're in the endgame, look further ahead based on the number of moves remaining.
-        if self.board.movesRemaining() <= 8:
-            print("Searching at depth 8")
-            move = self.minimax.minimax(self.team, 8, -1000000, 1000000)
-        elif self.board.movesRemaining() <= 10:
-            print("Searching at depth 5")
-            move = self.minimax.minimax(self.team, 5, -1000000, 1000000)
-        elif self.board.movesRemaining() <= 12:
-            print("Searching at depth 4")
-            move = self.minimax.minimax(self.team, 4, -1000000, 1000000)
-        else:
-            #If we're not in the endgame, use the number of valid moves to decide how deep to search
-            moveCount = self.board.getMoveCount(self.team)
-            if moveCount <= 5:
-                print("Searching at depth 5")
-                move = self.minimax.minimax(self.team, 5, -1000000, 1000000)
-            elif moveCount <= 9:
-                print("Searching at depth 4")
-                move = self.minimax.minimax(self.team, 4, -1000000, 1000000)
-            else:
-                print("Searching at depth 3")
-                move = self.minimax.minimax(self.team, 3, -1000000, 1000000)
+
+        move = []
+        start, depth = time(), 2
+        while (time() - start) < 0.8 and depth <= self.board.movesRemaining():
+            depth += 1
+            print("Searching at depth", depth)
+            start = time()
+            move = self.minimax.minimax(self.team, depth, -1000000, 1000000)
+            print("Search took:", (time() - start) * 1000, "ms\n")
         
         #Something wrong with the move returned
         if len(move) == 0 or len(move[1]) == 0:
